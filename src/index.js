837640452f5cfe9f12029fb32fe3eca18a89cfe3
Lesliee97 <eilsel1718rv@gmail.com>
2019-06-11 22:39:50 -0500

    actualizando js 11.06

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-const primeraPagina =document.getElementById("paginaUno");
-const segundaPagina =document.getElementById("paginaDos");
-const nombre = document.getElementById("clave").value;
-const confirmacion =document.getElementById("enviar");
+//variables de mis paginas y botones
+const primeraPagina = document.getElementById("paginaUno");
+const segundaPagina = document.getElementById("paginaDos");
+const terceraPagina = document.getElementById("paginaTres");
 const back = document.getElementById("Back");
-//funcion cambio de pagina
-primeraPagina.style.display = "block";
-segundaPagina.style.display = "none";
+let contador = 0;
+const enviar = document.getElementById("enviar");
 
-//funcion de mi boton
-confirmacion.addEventListener("click", function(){
-    primeraPagina.style.display = 'none';
-    segundaPagina.style.display = 'block';  
+//variables de mi cipher
+let cifrar = document.getElementById("cifrar");
+let result1 = document.getElementById("result1");
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let descifrar = document.getElementById("descifrar");
+let result2 = document.getElementById("result2");
+
+const btncifrar = document.getElementById("linea");
+const btndescifrar = document.getElementById("linea2");
+
+//funcion de mi log in 
+enviar.addEventListener("click", (event) => {
+    event.preventDefault();
+    const clave = document.getElementById("clave").value;
+
+    if (clave === "LABORATORIA") {
+        primeraPagina.classList.add("hide");
+        segundaPagina.classList.remove("hide");
+    }else {
+        contador = contador + 1;
+        document.getElementById("error").innerHTML = "Upss contraseña incorrecta"
+        if (contador === 3) {
+            document.getElementById("error").innerHTML = "Lo sentimos, ya no puede ingresar"
+        
+        }else {
+            
+       terceraPagina.classList.add("hide");
+       primeraPagina.classList.remove("hide");
+    }
+    console.log(contador);
+    }
 });
-//funcion para regresar a la primera pagina
-back.addEventListener("click",function(){
-    segundaPagina.style.display="none";
-    primeraPagina.style.display="block";
-})
 
 
+back.addEventListener("click", () => {
+    segundaPagina.style.display = "none";
+    primeraPagina.style.display = "block";
+});
