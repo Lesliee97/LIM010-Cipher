0bdf341d3323a049a178b0d008919a421fdd62f8
Lesliee97 <eilsel1718rv@gmail.com>
2019-06-20 09:46:11 -0500

    lang

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,11 @@
 window.cipher = {
   encode: (offset, string) => {
     let result1 = "";
-    for (let i = 0; i < string.length; i++) {
-      let ascii = string.charCodeAt(i);
-      if (65 <= ascii && ascii <= 90) {
-        result1+= String.fromCharCode((ascii - 65 + offset) % 26 + 65); 
-      } else if (ascii === 32) {
+    for (let i = 0; i < string.length; i++) { 
+      let ascii = string.charCodeAt(i); //convirtiendo a ascii
+      if (65 <= ascii && ascii <= 90) { //formula para las mayusculas
+        result1+= String.fromCharCode((ascii - 65 + offset) % 26 + 65); //formula del cifrado
+      } else if (ascii === 32) { // para que me reconozca espacios
         result1 += " ";
       }
     }
@@ -13,7 +13,7 @@ window.cipher = {
   },
   decode: (offset, string) => {
     let result2 = "";
-    for (let i = 0; i < string.length; i++){
+    for (let i = 0; i < string.length; i++){ 
       let ascii = string.charCodeAt(i);
       if (65 <= ascii && ascii <= 90) {
         result2 += String.fromCharCode((ascii + 65 - offset) % 26 + 65);
