33bdc4811fe61bfe67cdce51aad68ff260f9f777
Lesliee97 <eilsel1718rv@gmail.com>
2019-06-20 08:40:00 -0500

    final ghpages

--- src/cipher.js
+++ src/cipher.js
@@ -3,27 +3,11 @@ window.cipher = {
     let result1 = "";
     for (let i = 0; i < string.length; i++) {
       let ascii = string.charCodeAt(i);
-<<<<<<< HEAD
-      if (65 <= ascii && ascii <= 90)
-        result1 += String.fromCharCode((ascii - 65 + offset) % 26 + 65); // formula de desplazamiento de nuestro mensaje
-      else if (ascii === 32)
-        result1 += " ";
-=======
       if (65 <= ascii && ascii <= 90) {
         result1+= String.fromCharCode((ascii - 65 + offset) % 26 + 65); 
       } else if (ascii === 32) {
         result1 += " ";
-<<<<<<< HEAD
-<<<<<<< HEAD
       }
-
->>>>>>> 0bd8610fea0e7ab8fcd5b465820755ad34805edc
-=======
->>>>>>> 2504599a758751beb54c541b20470e6b30ceb6fb
-=======
-      }
-      
->>>>>>> 77d966119d7e78160694dfdb39a08163db40cd65
     }
     return result1;
   },
@@ -31,26 +15,13 @@ window.cipher = {
     let result2 = "";
     for (let i = 0; i < string.length; i++){
       let ascii = string.charCodeAt(i);
-<<<<<<< HEAD
-      if (65 <= ascii && ascii <= 90)
-        result2 += String.fromCharCode((ascii + 65 - offset) % 26 + 65);
-      else if (ascii === 32)
-        result2 += " ";
-=======
       if (65 <= ascii && ascii <= 90) {
         result2 += String.fromCharCode((ascii + 65 - offset) % 26 + 65);
       } else if (ascii === 32) {
         result2 += " ";
-<<<<<<< HEAD
-<<<<<<< HEAD
       }
->>>>>>> 0bd8610fea0e7ab8fcd5b465820755ad34805edc
-=======
->>>>>>> 2504599a758751beb54c541b20470e6b30ceb6fb
-=======
-      }
->>>>>>> 77d966119d7e78160694dfdb39a08163db40cd65
     }
     return result2;
   }
 };
+  
