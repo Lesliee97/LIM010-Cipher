77d966119d7e78160694dfdb39a08163db40cd65
Lesliee97 <eilsel1718rv@gmail.com>
2019-06-19 20:51:13 -0500

    cambiando nombre de mis variables

--- src/cipher.js
+++ src/cipher.js
@@ -4,22 +4,23 @@ window.cipher = {
     for (let i = 0; i < string.length; i++) {
       let ascii = string.charCodeAt(i);
       if (65 <= ascii && ascii <= 90) {
-        result1 += String.fromCharCode((ascii - 65 + offset) % 26 + 65); // formula de desplazamiento de nuestro mensaje
-      } else if (ascii === 32)
+        result1+= String.fromCharCode((ascii - 65 + offset) % 26 + 65); 
+      } else if (ascii === 32) {
         result1 += " ";
+      }
+      
     }
     return result1;
   },
-
-
   decode: (offset, string) => {
     let result2 = "";
-    for (let i = 0; i < string.length; i++) {
+    for (let i = 0; i < string.length; i++){
       let ascii = string.charCodeAt(i);
       if (65 <= ascii && ascii <= 90) {
         result2 += String.fromCharCode((ascii + 65 - offset) % 26 + 65);
-      } else if (ascii === 32)
+      } else if (ascii === 32) {
         result2 += " ";
+      }
     }
     return result2;
   }
